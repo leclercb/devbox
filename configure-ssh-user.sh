35d7390eff1b69f9b89edad61496bd894400ab1e
Benjamin Leclerc <benjamin.leclerc@me.com>
2024-11-05 13:38:39 +0000

    Fix code-server

--- configure-ssh-user.sh
+++ configure-ssh-user.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
 
+: ${USERNAME:=ubuntu}
+: ${PASSWORD:?"Error: PASSWORD environment variable is not set."}
+
 : ${ROOT_PASSWORD:?"Error: ROOT_PASSWORD environment variable is not set."}
-: ${SSH_USERNAME:=ubuntu}
-: ${SSH_PASSWORD:?"Error: SSH_PASSWORD environment variable is not set."}
 : ${CS_PASSWORD:?"Error: CS_PASSWORD environment variable is not set."}
+
 : ${AUTHORIZED_KEYS:=""}
+
 : ${GIT_USERNAME:=""}
 : ${GIT_EMAIL:=""}
 
+# Set root password
 echo "root:$ROOT_PASSWORD" | chpasswd
 
 # Create the user with the provided username and set the password
-if id "$SSH_USERNAME" &>/dev/null; then
-    echo "$SSH_USERNAME:$SSH_PASSWORD" | chpasswd
-    echo "User $SSH_USERNAME already exists"
+if id "$USERNAME" &>/dev/null; then
+    echo "$USERNAME:$PASSWORD" | chpasswd
+    echo "User $USERNAME already exists"
 else
-    useradd -ms /bin/bash "$SSH_USERNAME"
-    echo "$SSH_USERNAME:$SSH_PASSWORD" | chpasswd
-    echo "User $SSH_USERNAME created with the provided password"
+    useradd -ms /bin/bash "$USERNAME"
+    echo "$USERNAME:$PASSWORD" | chpasswd
+    echo "User $USERNAME created with the provided password"
 fi
 
 # Create home and workspace folders
-mkdir -p /home/$SSH_USERNAME/workspace
-chown -R $SSH_USERNAME:$SSH_USERNAME /home/$SSH_USERNAME
+mkdir -p /home/$USERNAME/workspace
+chown -R $USERNAME:$USERNAME /home/$USERNAME
 
 # Configure git
 if [ -n "$GIT_USERNAME" ]; then
-    su $SSH_USERNAME -c 'git config --global user.name "$GIT_USERNAME"'
+    su $USERNAME -c 'git config --global user.name "$GIT_USERNAME"'
 fi
 
 if [ -n "$GIT_EMAIL" ]; then
-    su $SSH_USERNAME -c 'git config --global user.email "$GIT_EMAIL"'
+    su $USERNAME -c 'git config --global user.email "$GIT_EMAIL"'
 fi
 
 # Install vscode-server
@@ -39,21 +43,21 @@ chmod +x download-vs-code.sh
 ./download-vs-code.sh "linux" "x64"
 rm download-vs-code.sh
 
-# Install code-server
+# Install and start code-server
 curl -fsSL https://code-server.dev/install.sh | sh
-su $SSH_USERNAME -c 'PASSWORD=$CS_PASSWORD nohup code-server --app-name=devbox --auth=password --user-data-dir=/home/$SSH_USERNAME/workspace &'
+su $USERNAME -c 'PASSWORD=$CS_PASSWORD nohup code-server --bind-addr=0.0.0.0:8080 --app-name=devbox --auth=password --user-data-dir=/home/$USERNAME/workspace &'
 
 # Configure ssh
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
 
 # Set the authorized keys from the AUTHORIZED_KEYS environment variable (if provided)
 if [ -n "$AUTHORIZED_KEYS" ]; then
-    mkdir -p /home/$SSH_USERNAME/.ssh
-    echo "$AUTHORIZED_KEYS" > /home/$SSH_USERNAME/.ssh/authorized_keys
-    chown -R $SSH_USERNAME:$SSH_USERNAME /home/$SSH_USERNAME/.ssh
-    chmod 700 /home/$SSH_USERNAME/.ssh
-    chmod 600 /home/$SSH_USERNAME/.ssh/authorized_keys
-    echo "Authorized keys set for user $SSH_USERNAME"
+    mkdir -p /home/$USERNAME/.ssh
+    echo "$AUTHORIZED_KEYS" > /home/$USERNAME/.ssh/authorized_keys
+    chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
+    chmod 700 /home/$USERNAME/.ssh
+    chmod 600 /home/$USERNAME/.ssh/authorized_keys
+    echo "Authorized keys set for user $USERNAME"
     # Disable password authentication if authorized keys are provided
     sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 fi
