93f0400b4251f89c261000f15ab71cfb6c2d87d5
Benjamin Leclerc <52453788+leclercb@users.noreply.github.com>
2025-06-10 23:07:18 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -61,7 +61,6 @@ fi
 
 # Call the setup script
 if [ -n "${SETUP_SCRIPT}" ] && [ ! -f "${SETUP_SCRIPT}" ]; then
-if [ -n "${SETUP_SCRIPT}" ]; then
     chmod +x "${SETUP_SCRIPT}"
     echo "Calling setup script"
     su ${USERNAME} -c '${SETUP_SCRIPT}'
