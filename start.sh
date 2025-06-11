95da919d3b402bec8cd6dedacf4c0757255f3da9
Benjamin Leclerc <benjamin.leclerc@me.com>
2025-06-11 07:10:52 +0000

    Improve start.sh

--- start.sh
+++ start.sh
@@ -16,37 +16,50 @@
 : ${SETUP_SCRIPT:="/home/${USERNAME}/.devbox/setup.sh"}
 
 # Set the root password
+echo "Set root password"
 echo "root:${ROOT_PASSWORD}" | chpasswd
 
 # Create the user with the provided username and set the password
 if id "${USERNAME}" &>/dev/null; then
-    echo "${USERNAME}:${PASSWORD}" | chpasswd
     echo "User ${USERNAME} already exists"
 else
     useradd -ms /bin/bash "${USERNAME}"
-    echo "${USERNAME}:${PASSWORD}" | chpasswd
     echo "User ${USERNAME} created with the provided password"
 fi
 
+# Configure user
+echo "Set user password"
+echo "${USERNAME}:${PASSWORD}" | chpasswd
+
+echo "Add user to sudoers"
+usermod -aG sudo ${USERNAME}
+echo "${USERNAME} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USERNAME}
+chmod 600 /etc/sudoers.d/${USERNAME}
+
 # Create home and workspace folders
+echo "Create home and workspace folders"
 mkdir -p /home/${USERNAME}/workspace
 chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}
 
 # Configure git
 if [ -n "${GIT_USERNAME}" ]; then
-    su ${USERNAME} -c 'git config --global user.name "${GIT_USERNAME}"'
+    echo "Set git username"
+    su ${USERNAME} -c "git config --global user.name '${GIT_USERNAME}'"
 fi
 
 if [ -n "${GIT_EMAIL}" ]; then
-    su ${USERNAME} -c 'git config --global user.email "${GIT_EMAIL}"'
+    echo "Set git email"
+    su ${USERNAME} -c "git config --global user.email '${GIT_EMAIL}'"
 fi
 
 # Start code-server
 if [ -n "${CS_PASSWORD}" ]; then
-    su ${USERNAME} -c 'PASSWORD=${CS_PASSWORD} nohup code-server --bind-addr=0.0.0.0:8080 --app-name=devbox --auth=password &'
+    echo "Start code-server"
+    su ${USERNAME} -c "PASSWORD='${CS_PASSWORD}' nohup code-server --bind-addr=0.0.0.0:8080 --app-name=devbox --auth=password &"
 fi
 
 # Create .ssh folder
+echo "Create .ssh folder"
 mkdir -p /home/${USERNAME}/.ssh
 chmod 700 /home/${USERNAME}/.ssh
 chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/.ssh
@@ -61,9 +74,9 @@ fi
 
 # Call the setup script
 if [ -n "${SETUP_SCRIPT}" ] && [ -f "${SETUP_SCRIPT}" ]; then
-    chmod +x "${SETUP_SCRIPT}"
     echo "Calling setup script"
-    su ${USERNAME} -c '${SETUP_SCRIPT}'
+    chmod +x ${SETUP_SCRIPT}
+    su ${USERNAME} -c ${SETUP_SCRIPT}
 fi
 
 # Start the SSH server
